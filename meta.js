219286702ac5e025d66b17762eba4b043cbf922d
sunmengmeng <mengmeng.smm@alibaba-inc.com>
2017-05-08 18:30:58 +0800

    fix

--- meta.js
+++ meta.js
@@ -22,8 +22,5 @@ module.exports = {
       "message": "use dll(可以支持长缓存和快速打包)?"
     }
   },
-  {
-    "skipInterpolation": "src/**/*.vue"
-  },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dll(if install)\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
